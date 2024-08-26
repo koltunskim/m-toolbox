fbf197839db1d0e5b90753fc2e1168e4013fc378
koltunskim <koltunski.m@gmail.com>
2024-08-26 12:35:03 -0400

    fix apt failing

--- src/scripts/install_nvim.sh
+++ src/scripts/install_nvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #install dependencies
+apt-get update -y
 apt-get -y install curl ripgrep tree git xclip python3 python3-pip nodejs npm tzdata ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config zip unzip
 
 #build from source
