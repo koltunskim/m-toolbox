cb93988ad91f562f10ed6c489ddb73942349387a
koltunskim <koltunski.m@gmail.com>
2024-08-26 13:11:06 -0400

    fix bootstrap errors

--- src/scripts/bootstrap.sh
+++ src/scripts/bootstrap.sh
@@ -3,8 +3,10 @@
 export DEBIAN_FRONTEND='noninteractive'
 
 script_dir=$(dirname $(readlink --canonicalize "$0"))
-all_scripts=$(find "${script_dir}" -name "*.sh" -not -name "bootstrap.sh")
+all_scripts=$(find "${script_dir}" -name "*.sh" -not -name "bootstrap.sh" | sort)
 
 for script in "${all_scripts}"; do 
+	echo "scripts to run: "${all_scripts}"
+	echo "now running: "${script}"
 	/bin/bash -c "${script}"
 done
