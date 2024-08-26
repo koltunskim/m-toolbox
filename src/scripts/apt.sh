57a62e988020ebfd12b784920e59fd638e6620c0
koltunskim <koltunski.m@gmail.com>
2024-08-26 13:10:22 -0400

    move apt to own script

---
+++ src/scripts/apt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get -y update
+
+#vim dependencies
+apt-get -y install curl ripgrep tree git xclip python3 python3-pip nodejs npm tzdata ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config zip unzip
+
+#tools
+apt-get -y install vim tmux curl dnsutils ripgrep
--- src/scripts/install_nvim.sh
+++ src/scripts/install_nvim.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#install dependencies
-apt-get update -y
-apt-get -y install curl ripgrep tree git xclip python3 python3-pip nodejs npm tzdata ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config zip unzip
-
 #build from source
 cd /tmp && git clone https://github.com/neovim/neovim
 cd /tmp/neovim && git checkout stable && make -j4 && make install
