4e122d7aea2ea9a7b415a75aeb852f0c7d92f64f
koltunskim <koltunski.m@gmail.com>
2024-08-31 17:17:00 +0200

    add vim dependcies

--- src/scripts/apt.sh
+++ src/scripts/apt.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 
 #vim dependencies
-apt-get -y install curl ripgrep tree git xclip python3 python3-pip nodejs npm tzdata ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config zip unzip
+apt-get -y install curl ripgrep tree git xclip python3 python3-pip python3-all-venv nodejs npm tzdata ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config zip unzip
 
 #tools
 apt-get -y install vim tmux curl dnsutils ripgrep
