c37bf126819dc973db827d5e3b797b33235d7fcb
koltunskim <koltunski.m@gmail.com>
2024-09-26 15:17:16 +0200

    feat: add tenv

---
+++ src/scripts/install_tenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+LATEST_VERSION=$(curl --silent https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)
+curl -O -L "https://github.com/tofuutils/tenv/releases/latest/download/tenv_${LATEST_VERSION}_amd64.deb"
+sudo dpkg -i "tenv_${LATEST_VERSION}_amd64.deb"
